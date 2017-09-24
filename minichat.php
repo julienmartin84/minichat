bf5ed282f2d2764b4feb15b8ced69e12b5a604ac
julienmartin84 <martinjulienweb@gmail.com>
2017-09-24 09:52:49 +0200

    Typo error corrected

--- minichat.php
+++ minichat.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>minichat</title>
-        <meta charset=utf-8" />
+        <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="minichatstyle.css">
         <script src="minichat.js"></script>
     </head>
